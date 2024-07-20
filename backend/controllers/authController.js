2e508a1648adb12b58133543f0926901fe22cc6a
NatnaleKete <ketemanatnael023@gmail.com>
2024-07-20 00:10:25 -0700

    1-backend completed

---
+++ backend/controllers/authController.js
@@ -0,0 +1,80 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+import User from "../models/userModel.js";
+import { generateToken } from "../utility/generateToken.js";
+
+export const createAccount = asyncHandler(async (req, res, next) => {
+  try {
+    const { fullName, username, email, password, gender } = req.body;
+    const existUser = await User.findOne({ username });
+    const existEmail = await User.findOne({ email });
+
+    if (existUser) {
+      res.status();
+      throw new Error("Username is already taken");
+    }
+
+    if (existEmail) {
+      res.status();
+      throw new Error("User already registered");
+    }
+
+    const newUser = await User.create({
+      fullName,
+      username,
+      email,
+      password,
+      gender,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+export const LoginUsers = asyncHandler(async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (user && (await user.matchPassword(password))) {
+      generateToken(user._id, res);
+
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        email: user.email,
+        profilePic: user.profilePic,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+export const logout = asyncHandler(async (req, res, next) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
